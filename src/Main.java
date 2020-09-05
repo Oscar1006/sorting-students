f336e76b348c7b5fb91fa24cd8c61464fd0ac718
Oscar1006 <54926672+Oscar1006@users.noreply.github.com>
2020-09-04 23:07:21 -0500

    Creation and fill the matrix

--- src/Main.java
+++ src/Main.java
@@ -7,15 +7,29 @@ public class Main {
         int m = 0;
         String ages = "";
         n = Integer.parseInt(s.nextLine());
-
+        //Read all values
         for(int i = 0; i < n; i++){
             ages += s.nextLine() + "-";
         }
+        //Split the rows
         String[] agesRows = ages.split("-");
+        //Get the measure of the longer row
         for(int i = 0; i < n; i++){
             if (m < agesRows[i].length()){
-                String[] separated = 
+                String[] separated = agesRows[i].split("// s");
+                m = separated.length; 
             }
         }
+        //Create the matrix
+        double[][] matrix = new double[n][m];
+        //Fill the matrix
+        for(int i = 0; i < n; i++){
+            String[] numbers = agesRows[i].split("// s");
+            for(int j = 0; j < numbers.length; j++){
+                matrix[i][j] = Double.parseDouble(numbers[j]);
+            }
+        }
+        
+
     }
 }
